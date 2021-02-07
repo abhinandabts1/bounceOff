5a1ed76065d9639aa8cf0e8932a746aeafd53b60
abhinandabts1 <76810646+abhinandabts1@users.noreply.github.com>
2021-02-07 11:00:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,71 @@
+var fixedRect, movingRect, gameObject1,gameObject2,gameObject3,gameObject4;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  // fixedRect.debug = true;
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  // movingRect.debug = true;
+
+  gameObject1 = createSprite(100,100,50,50);
+  gameObject1.shapeColor = "green";
+  gameObject2 = createSprite(200,100,50,50);
+  gameObject2.shapeColor = "green";
+  gameObject3 = createSprite(100,650,50,50);
+  gameObject3.shapeColor = "green";
+  gameObject4 = createSprite(1100,650,50,50);
+  gameObject4.shapeColor = "blue";
+  gameObject3.velocityX = 5;
+  gameObject4.velocityX = -5;
+
+
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+if(isTouching(movingRect,gameObject1) || isTouching(movingRect,fixedRect)){
+  textSize(30);
+  text("Yeiiii Toched",100,200);
+}
+bounceOff(gameObject3, gameObject4);
+  drawSprites();
+}
+
+function isTouching(object1,object2){
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2
+    && object1.y - object2.y < object2.height/2 + object1.height/2
+    && object2.y - object1.y < object2.height/2 + object1.height/2) {
+      //In computer language, "yes" and "no" are written as true and false.
+      // These are called boolean values.
+  return true;
+}
+else {
+  return false;
+}
+}
+
+function bounceOff (object1,object2) {
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2){
+
+//10*(-1)=-10
+//-10*(-1)=10
+  object1.velocityX = object1.velocityX * (-1);     
+  object2.velocityX = object2.velocityX * (-1);
+
+    }
+if (object1.y - object2.y < object2.height/2 + object1.height/2
+  && object2.y - object1.y < object2.height/2 + object1.height/2){
+
+object1.velocityY = object1.velocityY * (-1);
+object2.velocityY = object2.velocityY * (-1);
+  }
+
+
+}
